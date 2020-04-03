43e81aeb33adebefbac00cd87bf3d7f3be72c5fc
Temir Erenchin <t.erenchin@hotmail.com>
2020-04-03 20:00:48 +0700

    init

---
+++ rss_client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rss_client
+//
+//  Created by temir on 03.04.2020.
+//  Copyright © 2020 temir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
